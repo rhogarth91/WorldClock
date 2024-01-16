28c5e9614dd7e2294192d85e31de3bc94fdda10a
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2024-01-16 17:50:36 +0000

    JS updated to show live time and date data
    -Moment CDN used for live data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("dddd Do MMMM YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "H:mm:ss [<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("dddd Do MMMM YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "H:mm:ss [<small>]A[</small>]"
+  );
+}
+
+setInterval(updateTime, 1000);
+updateTime();
