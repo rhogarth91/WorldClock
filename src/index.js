822bf7e3863050fa448c3ab893e37d0c6c72de09
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2024-01-18 16:45:31 +0000

    Added function to show time in multiple cities as well as current timezone of user

--- src/index.js
+++ src/index.js
@@ -1,24 +1,54 @@
 function updateTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("dddd Do MMMM YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "H:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML =
+      losAngelesTime.format("dddd Do MMMM YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "H:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  sydneyTime = moment().tz("Australia/Sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    sydneyTime = moment().tz("Australia/Sydney");
 
-  sydneyDateElement.innerHTML = sydneyTime.format("dddd Do MMMM YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "H:mm:ss [<small>]A[</small>]"
-  );
+    sydneyDateElement.innerHTML = sydneyTime.format("dddd Do MMMM YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "H:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
-setInterval(updateTime, 1000);
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  <a href="/"> Back </a>  
+  `;
+}
+
 updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#selected-city");
+citiesSelectElement.addEventListener("change", updateCity);
